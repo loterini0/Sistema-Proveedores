347ccaebe509f8f1c472d44af10fbf31b1127e0e
Samuel Moncada Salazar <samoncadas@unal.edu.co>
2026-07-22 12:51:28 -0500

    feat: add restoreSession

--- apps/mobile/src/store/auth.store.ts
+++ apps/mobile/src/store/auth.store.ts
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import * as SecureStore from "expo-secure-store";
+import { authService } from "../services/api";
 
-interface User {
+export interface User {
   id: string;
   email: string;
   nombre: string;
@@ -11,6 +12,7 @@ interface User {
 interface AuthState {
   user: User | null;
   isAuthenticated: boolean;
+  isHydrated: boolean;
   setUser: (
     user: User,
     accessToken: string,
@@ -23,6 +25,36 @@ interface AuthState {
 export const useAuthStore = create<AuthState>((set, get) => ({
   user: null,
   isAuthenticated: false,
+  isHydrated: false,
+  restoreSession: async() => {
+    try{
+      const accessToken = await SecureStore.getItemAsync("accessToken")
+
+      if(!accessToken){
+        return;
+      }
+
+      const {data} = await authService.me();
+      const user = data.user;
+
+      set ({
+        user:{
+          id: user.id ?? user.userId,
+          nombre: user.nombre,
+          email: user.email,
+          empresaId: user.empresaId ?? null,
+        },
+        isAuthenticated: true,
+      });
+    } catch{
+      await SecureStore.deleteItemAsync("accessToken");
+      await SecureStore.deleteItemAsync("refreshToken");
+      set({user:null, isAuthenticated: false});
+    } finally{
+      set({isHydrated: true});
+    }
+  },
+  
   setUser: async (user, accessToken, refreshToken) => {
     await SecureStore.setItemAsync("accessToken", accessToken);
     await SecureStore.setItemAsync("refreshToken", refreshToken);
