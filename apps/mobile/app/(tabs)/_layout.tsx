d15e28d31bff4631a1abe406c5beaaaea153c356
Samuel Moncada Salazar <samoncadas@unal.edu.co>
2026-07-23 10:29:11 -0500

    feat tabs -> _layout

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,18 +1,45 @@
 import { Tabs } from "expo-router";
+import {Building2, ClipboardList, House, User } from "lucide-react-native"
+import { colors } from "../../src/theme/colors";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: colors.primary, tabBarInactiveTintColor: colors.textSecondary }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Inicio",
+          tabBarIcon: ({ color, size }) => <House color = {color} size = {size}/>
+        }}
+      />
+
+      <Tabs.Screen
+        name="empresas"
+        options={{
+          title: "Empresas",
+          tabBarIcon: ({ color, size }) => (
+            <Building2 color={color} size={size}/>
+          )
+        }}
+      />
+      
       <Tabs.Screen
         name="rfqs"
         options={{
           title: "RFQs",
+          tabBarIcon: ({ color, size }) => (
+            <ClipboardList color={color} size={size}/>
+          )
         }}
       />
-
-      {/* En el futuro puedes agregar más pestañas */}
-      {/* <Tabs.Screen name="empresas" options={{ title: "Empresas" }} /> */}
-      {/* <Tabs.Screen name="perfil" options={{ title: "Perfil" }} /> */}
+      
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color, size }) => <User color={color} size = {size}/>
+        }}
+      />
     </Tabs>
   );
 }
