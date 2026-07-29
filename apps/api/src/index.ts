b59504de0d19872b04851826bd412e7e39e21f5d
loterini0 <jloteror@unal.edu.co>
2026-07-29 02:55:37 -0500

    debug: log database url configuration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import app from './app';
 
+console.log('DATABASE_URL configured:', !!process.env.DATABASE_URL);
+console.log('DB host:', process.env.DATABASE_URL?.split('@')[1]?.split('/')[0]);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
